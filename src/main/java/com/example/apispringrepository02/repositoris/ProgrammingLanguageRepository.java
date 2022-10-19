f965e89e794ebc9c0ac46e29f3ec1c31e340e963
Domenico <menico.c90@gmail.com>
2022-10-19 20:36:14 +0200

    feat: finish

--- src/main/java/com/example/apispringrepository02/repositoris/ProgrammingLanguageRepository.java
+++ src/main/java/com/example/apispringrepository02/repositoris/ProgrammingLanguageRepository.java
@@ -2,11 +2,9 @@ package com.example.apispringrepository02.repositoris;
 
 import com.example.apispringrepository02.entities.ProgrammingLanguage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-
-@RepositoryRestResource
-@RequestMapping(path = "/repo-prog-languages", headers = "Is a item")
+@RepositoryRestResource(path = "repo-prog-languages", itemResourceDescription = @Description("dkfjkgjkf"))
 public interface ProgrammingLanguageRepository extends JpaRepository<ProgrammingLanguage, Long> {
 }
